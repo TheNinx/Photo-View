9c116e3a101a7700880367f68e75002923174fa6
Thewe <the.well@hotmail.com>
2022-04-30 10:54:56 -0300

    Firt Commit alurapic

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.ts.html',
+
+})
+
+export class PhotoComponent{
+
+  description = 'Camila atualmente é uma grade investidora anjo no ramo da técnologia';
+  url = 'http://simaigualdaderacial.com.br/jantar/wp-content/uploads/2019/05/Camila-Farani.jpg';
+}
