227306116973498ec14736ba6080435de2a9d178
theninx <the.well@hotmail.com>
2022-05-02 18:39:51 -0300

    Implementação photo.component.ts

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -8,6 +8,6 @@ import {Component,Input} from "@angular/core";
 
 export class PhotoComponent{
 
-   @Input() description = '';
-   @Input() url = '';
+  @Input() description = '';
+  @Input() url = '';
 }
